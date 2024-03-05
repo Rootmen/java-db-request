238638bbf631c43b260af9b4205d39d9fcc229c1
Rootmen <10poma10@mail.ru>
2024-03-05 19:49:45 +0300

    static for quarkus build

--- sourse/modules/database-controller/src/main/java/ru/iedt/database/request/controller/DatabaseController.java
+++ sourse/modules/database-controller/src/main/java/ru/iedt/database/request/controller/DatabaseController.java
@@ -22,10 +22,10 @@ import java.util.*;
 
 @Singleton
 public class DatabaseController {
-    private final Map<String, Elements.Definition> QUERY_STORE_DEFINITION_MAP = new HashMap<>();
+    private final static Map<String, Elements.Definition> QUERY_STORE_DEFINITION_MAP = new HashMap<>();
     private final static ArrayList<QueryStoreDefinition> INITIALIZERS  = QueryStoreList.getStoresMetadata();;
 
-    DatabaseController() {
+    static  {
         for (QueryStoreDefinition queryStoreDefinition : INITIALIZERS) {
             try {
                 String storeName = queryStoreDefinition.getStoreName();
