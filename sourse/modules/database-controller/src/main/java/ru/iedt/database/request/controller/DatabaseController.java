c1be58b4a58aeb9f3fbee3f10f3b309c0216e207
Rootmen <10poma10@mail.ru>
2024-03-12 21:14:29 +0300

    new error log

--- sourse/modules/database-controller/src/main/java/ru/iedt/database/request/controller/DatabaseController.java
+++ sourse/modules/database-controller/src/main/java/ru/iedt/database/request/controller/DatabaseController.java
@@ -7,6 +7,8 @@ import io.vertx.mutiny.sqlclient.Row;
 import io.vertx.mutiny.sqlclient.RowSet;
 import io.vertx.mutiny.sqlclient.Tuple;
 import jakarta.inject.Singleton;
+import java.io.InputStream;
+import java.util.*;
 import ru.iedt.database.request.controller.parameter.ParameterInput;
 import ru.iedt.database.request.parser.elements.v3.ParserEngine;
 import ru.iedt.database.request.store.QueryStoreDefinition;
@@ -14,18 +16,11 @@ import ru.iedt.database.request.store.QueryStoreList;
 import ru.iedt.database.request.structures.nodes.v3.Elements;
 import ru.iedt.database.request.structures.nodes.v3.node.SQL;
 
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.*;
-
 @Singleton
 public class DatabaseController {
+
     private static final Map<String, Elements.Definition> QUERY_STORE_DEFINITION_MAP = new HashMap<>();
     private static final ArrayList<QueryStoreDefinition> INITIALIZERS = QueryStoreList.getStoresMetadata();
-    ;
 
     static {
         for (QueryStoreDefinition queryStoreDefinition : INITIALIZERS) {
@@ -33,16 +28,13 @@ public class DatabaseController {
             if (QUERY_STORE_DEFINITION_MAP.containsKey(storeName)) {
                 throw new RuntimeException("Несколько хранилищ с одинаковым названием!");
             }
-            InputStream file = queryStoreDefinition
-                    .getResourceClass()
-                    .getResourceAsStream(queryStoreDefinition.getResourcePatch());
+            InputStream file = queryStoreDefinition.getResourceClass().getResourceAsStream(queryStoreDefinition.getResourcePatch());
             Elements.Definition definition = ParserEngine.parsingXml(file);
             QUERY_STORE_DEFINITION_MAP.put(storeName, definition);
         }
     }
 
-    public Uni<List<Map<String, RowSet<Row>>>> runningQuerySet(
-            String storeName, String queryName, Map<String, ParameterInput> parameterInputs, PgPool client) {
+    public Uni<List<Map<String, RowSet<Row>>>> runningQuerySet(String storeName, String queryName, Map<String, ParameterInput> parameterInputs, PgPool client) {
         Elements.Definition definition = QUERY_STORE_DEFINITION_MAP.get(storeName);
         if (definition == null) throw new RuntimeException("Хранилище Definition не найдено");
         Elements.QuerySet querySet = definition.getQuerySet().get(queryName);
@@ -51,13 +43,12 @@ public class DatabaseController {
         List<Elements.Queries> queries = querySet.getQueries();
         List<Uni<Map<String, RowSet<Row>>>> unis = new ArrayList<>();
         for (Elements.Queries query : queries) {
-            unis.add(runQueries(query, parameters, template, client));
+            unis.add(runQueries(query, parameters, template, client, storeName, queryName));
         }
         return Uni.join().all(unis).andCollectFailures();
     }
 
-    private Map<String, Elements.Parameter<?>> createParameters(
-            Map<String, ParameterInput> parameterInputs, Map<String, Elements.Parameter<?>> parameterMap) {
+    private Map<String, Elements.Parameter<?>> createParameters(Map<String, ParameterInput> parameterInputs, Map<String, Elements.Parameter<?>> parameterMap) {
         for (Map.Entry<String, Elements.Parameter<?>> entry : parameterMap.entrySet()) {
             String key = entry.getKey();
             Elements.Parameter<?> parameters = entry.getValue();
@@ -67,11 +58,7 @@ public class DatabaseController {
         return parameterMap;
     }
 
-    private Uni<Map<String, RowSet<Row>>> runQueries(
-            Elements.Queries queries,
-            Map<String, Elements.Parameter<?>> parameters,
-            Map<String, Elements.Template> template,
-            PgPool client) {
+    private Uni<Map<String, RowSet<Row>>> runQueries(Elements.Queries queries, Map<String, Elements.Parameter<?>> parameters, Map<String, Elements.Template> template, PgPool client, String storeName, String queryName) {
         List<Elements.SQL> sqlList = queries.getSql();
         List<SQL.InsertData> insertDataArray = new ArrayList<>();
         for (Elements.SQL sql : sqlList) {
@@ -87,17 +74,29 @@ public class DatabaseController {
                     parameter.addToTuple(tuple);
                 }
                 PreparedQuery<RowSet<Row>> preparedQuery = connection.preparedQuery(insertData.getSql());
-                Uni<RowSet<Row>> query = preparedQuery.execute(tuple);
+                Uni<RowSet<Row>> query = preparedQuery
+                    .execute(tuple)
+                    .onFailure()
+                    .invoke(throwable -> {
+                        System.err.println("Хранилище : " + storeName);
+                        System.err.println("Запрос : " + queryName);
+                        System.err.println("Текст запроса : " + insertData.getSql());
+                        throwable.printStackTrace();
+                    });
                 unisKey.add(insertData.getName());
                 unis.add(query);
             }
-            return Uni.combine().all().unis(unis).with(responses -> {
-                Map<String, RowSet<Row>> map = new LinkedHashMap<>();
-                for (int g = 0; g < responses.size(); g++) {
-                    map.put(unisKey.get(g), (RowSet<Row>) responses.get(g));
-                }
-                return map;
-            });
+            return Uni
+                .combine()
+                .all()
+                .unis(unis)
+                .with(responses -> {
+                    Map<String, RowSet<Row>> map = new LinkedHashMap<>();
+                    for (int g = 0; g < responses.size(); g++) {
+                        map.put(unisKey.get(g), (RowSet<Row>) responses.get(g));
+                    }
+                    return map;
+                });
         });
     }
 }
