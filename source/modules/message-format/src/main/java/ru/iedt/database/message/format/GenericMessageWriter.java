3878694849a90c7777832bb1ed1c5332d4f9ca03
Rootmen <10poma10@mail.ru>
2024-08-10 19:05:13 +0300

    Использование GitHubPackages

---
+++ source/modules/message-format/src/main/java/ru/iedt/database/message/format/GenericMessageWriter.java
@@ -0,0 +1,137 @@
+package ru.iedt.database.message.format;
+
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.tuples.Tuple2;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import ru.iedt.database.controller.TaskDescription;
+import ru.iedt.database.messaging.WebsocketMessage;
+import ru.iedt.database.messaging.WebsocketRequest;
+
+import static ru.iedt.database.message.format.MessageUtils.SUCCEED_MESSAGE;
+
+public class GenericMessageWriter {
+
+  public static <T> Uni<Void> sendMessagesFromMulti(
+      Uni<Tuple2<Integer, Multi<T>>> result, WebsocketMessage message, TaskDescription task) {
+    return sendMessagesFromMulti(result, message, task, TARGET_SOCKET);
+  }
+
+  static ExecutorService executorService = Executors.newFixedThreadPool(100);
+
+  public static <T> Uni<Void> sendMessagesFromMulti(
+      Uni<Tuple2<Integer, Multi<T>>> result,
+      WebsocketMessage message,
+      TaskDescription task,
+      String target) {
+    AtomicInteger index = new AtomicInteger(0);
+    return result
+        .onItem()
+        .transformToUni(
+            tuple ->
+                message
+                    .message(
+                        WebsocketRequest.newBuilder()
+                            .setUser(task.user_id.toString())
+                            .setSocket(task.socket)
+                            .setTarget(target)
+                            .setTaskName(task.task_name)
+                            .setTaskId(task.task_id.toString())
+                            .setPayload(MessageUtils.startMessage(tuple.getItem1()))
+                            .build())
+                    .replaceWith(tuple.getItem2()))
+        .onItem()
+        .transformToMulti(tMulti -> tMulti)
+        .emitOn(executorService)
+        .onItem()
+        .transformToUni(
+            t ->
+                message.message(
+                    WebsocketRequest.newBuilder()
+                        .setUser(task.user_id.toString())
+                        .setSocket(task.socket)
+                        .setTarget(target)
+                        .setTaskName(task.task_name)
+                        .setTaskId(task.task_id.toString())
+                        .setPayload(MessageUtils.rowMessage(index.getAndIncrement(), t))
+                        .build()))
+        .merge()
+        .select()
+        .where(i -> false)
+        .toUni()
+        .onItem()
+        .transformToUni(
+            unused ->
+                message.message(
+                    WebsocketRequest.newBuilder()
+                        .setUser(task.user_id.toString())
+                        .setSocket(task.socket)
+                        .setTarget(target)
+                        .setTaskName(task.task_name)
+                        .setTaskId(task.task_id.toString())
+                        .setPayload(MessageUtils.endMessage())
+                        .build()))
+        .replaceWithVoid();
+  }
+
+  public static <T> Uni<Void> sendMessagesFromUni(
+      Uni<T> result, WebsocketMessage message, TaskDescription task, String target) {
+    return result
+        .onItem()
+        .transformToUni(
+            t ->
+                message.message(
+                    WebsocketRequest.newBuilder()
+                        .setUser(task.user_id.toString())
+                        .setSocket(task.socket)
+                        .setTarget(target)
+                        .setTaskName(task.task_name)
+                        .setTaskId(task.task_id.toString())
+                        .setPayload(MessageUtils.message(t))
+                        .build()))
+        .replaceWithVoid();
+  }
+
+  public static Uni<Void> sendErrorMessage(
+      String errorMessage, WebsocketMessage message, TaskDescription task) {
+    return Uni.createFrom()
+        .voidItem()
+        .onItem()
+        .transformToUni(
+            t ->
+                message.message(
+                    WebsocketRequest.newBuilder()
+                        .setUser(task.user_id.toString())
+                        .setSocket(task.socket)
+                        .setTarget(TARGET_SOCKET)
+                        .setTaskName(task.task_name)
+                        .setTaskId(task.task_id.toString())
+                        .setPayload(MessageUtils.errorMessage(errorMessage))
+                        .build()))
+        .replaceWithVoid();
+  }
+
+  public static Uni<Void> sendSucceedMessage(WebsocketMessage message, TaskDescription task) {
+    return Uni.createFrom()
+        .voidItem()
+        .onItem()
+        .transformToUni(
+            t ->
+                message.message(
+                    WebsocketRequest.newBuilder()
+                        .setUser(task.user_id.toString())
+                        .setSocket(task.socket)
+                        .setTarget(TARGET_SOCKET)
+                        .setTaskName(task.task_name)
+                        .setTaskId(task.task_id.toString())
+                        .setPayload(SUCCEED_MESSAGE)
+                        .build()))
+        .replaceWithVoid();
+  }
+
+  public static String TARGET_ALL = "all";
+  public static String TARGET_SOCKET = "socket";
+}
---
+++ source/modules/message-format/src/main/java/ru/iedt/database/message/format/MessageUtils.java
@@ -0,0 +1,94 @@
+package ru.iedt.database.message.format;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+public class MessageUtils {
+
+  public static String SUCCEED_MESSAGE = "{\"succeed\":true}";
+  public static String INIT_MESSAGE = "{\"status\":0}";
+
+  public static ObjectMapper mapper = new ObjectMapper();
+
+  static {
+    mapper.registerModule(new JavaTimeModule());
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    mapper.setVisibility(
+        mapper
+            .getSerializationConfig()
+            .getDefaultVisibilityChecker()
+            .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+            .withIsGetterVisibility(JsonAutoDetect.Visibility.NONE)
+            .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+            .withSetterVisibility(JsonAutoDetect.Visibility.NONE));
+  }
+
+  public static String errorMessage(String error) {
+    if (error == null) error = "error is null";
+    return String.format("{\"succeed\":false,\"error\":\"%s\"}", error.replace("\"", "\\\""));
+  }
+
+  public static String message(Object message) {
+    try {
+      return String.format("{\"succeed\":true,\"result\":%s}", mapper.writeValueAsString(message));
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  // ------------------------------------------
+  public static String startMessage(int rowCount) {
+    try {
+      return String.format("{\"status\":1,\"row_count\":%s}", mapper.writeValueAsString(rowCount));
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static String rowMessage(int index, Object message) {
+    try {
+      return mapper.writeValueAsString(new RowLine(index, message));
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static String SUCCEED_PACKET_MESSAGE = "{\"status\": 3,\"succeed\":true}";
+
+  public static String endMessage() {
+    return SUCCEED_PACKET_MESSAGE;
+  }
+
+  public static RowDelete rowDeleted(Object message) {
+    return new RowDelete(message);
+  }
+
+  @RegisterForReflection
+  public static class RowLine {
+
+    public int status = 2;
+    public final int index;
+    public final Object result;
+
+    public RowLine(int index, Object result) {
+      this.index = index;
+      this.result = result;
+    }
+  }
+
+  @RegisterForReflection
+  public static class RowDelete {
+
+    public int status = 4;
+    public final Object deleted_id;
+
+    public RowDelete(Object deleted_id) {
+      this.deleted_id = deleted_id;
+    }
+  }
+}
